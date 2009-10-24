cc70308e71d614c42822ef01ba68d5d4e923d1bc
Christopher Thomas <chris@whitestar.local>
2009-10-24 08:45:50 +0200

    add the new database, translator, default translator interface, updated amslib ccore, a new split box model stylesheets for all important browsers, new prototype version, a DNI/NIE validator object and an updated form validator object

--- amslib.php
+++ amslib.php
@@ -1,10 +1,10 @@
 <?php
 /*******************************************************************************
- * Copyright (c) {06/11/2005} {Christopher Thomas} 
+ * Copyright (c) {15/03/2008} {Christopher Thomas} 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * version 3 of the License, or any later version.
  * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,21 +15,34 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  * 
- * Title: amslib
- *
- * Contributors:
- *    {Christopher Thomas} - Creator - chris.alex.thomas@gmail.com
+ * File: amslib.php
+ * Title: Amslib core utility object
+ * Version: 1.0
+ * Project: amslib
+ * 
+ * Contributors/Author:
+ *    {Christopher Thomas} - Creator - chris.thomas@antimatter-studios.com
  *******************************************************************************/
+
+//	PROBABLY REMOVE THIS
+$page = basename($_SERVER["PHP_SELF"]);
 
 class amslib
 {
+	function showErrors()
+	{
+		//	Enable all error reporting
+		ini_set("display_errors", "On");
+		error_reporting(E_ALL);
+	}
+	
 	/**
-	 * function:	addIncludePath
-	 * 
-	 * Add an include path to the PHP include path
-	 * 
-	 * parameters:
-	 * 	path	-	The path to add to the PHP include path
+	 *	function:	addIncludePath
+	 *
+	 *	Add an include path to the PHP include path
+	 *
+	 *	parameters:
+	 *		path	-	The path to add to the PHP include path
 	 */
 	function addIncludePath($path)
 	{
@@ -43,39 +56,40 @@ class amslib
 		if($valid) array_unshift($includePath,$path);
 		ini_set("include_path",implode(PATH_SEPARATOR,$includePath));
 	}
+
+	function getIncludeContents($filename)
+	{
+		ob_start();
+		include($filename);
+		$contents = ob_get_contents();
+		ob_end_clean();
+		return $contents;
+	}
 	
-	function resolveIncludePath($file)
+	function getVarDump($dump)
+	{
+		ob_start();
+		var_dump($dump);
+		$dump = ob_get_contents();
+		ob_end_clean();
+	
+		return $dump;
+	}
+	
+	function include_file($file,$showError=false)
 	{
 		$includePath = explode(PATH_SEPARATOR,ini_get("include_path"));
 		
 		foreach($includePath as $p){
-			$str = "$p/$file";
-			if(file_exists($str)) return $str;
+			$test = (strpos($file,"/") !== 0) ? "$p/$file" : "{$p}{$file}";
+			if(file_exists($test)){
+				return @include_once($test);
+			}	
 		}
 		
-		return NULL;
-	}
-	
-	function getIncludeContents($filename){
-		$output = "";
+		if($showError) print("INCLUDE PATH($file) = <pre>".print_r($includePath,true)."</pre>");
 		
-		if(file_exists($filename)){
-			ob_start();
-			$buffer = ob_get_contents();
-			include($filename);
-			$output = substr(ob_get_contents(),strlen($buffer));
-			ob_end_clean();
-		}		
-		return $output;
-	}
-	
-	function getVarDump($mixed = null)
-	{
-		ob_start();
-		var_dump($mixed);
-		$content = ob_get_contents();
-		ob_end_clean();
-		return $content;
+		return false;
 	}
 	
 	/**
@@ -218,16 +232,17 @@ class amslib
 	 * 		value	-	The value requested
 	 * 		return	-	The value to return if the value does not exist
 	 * 		erase	-	Whether or not to erase the value after it's been read
+	 * 		order	-	A two element array specifying what order to obtain the parameter in { get, post } is the default order
 	 * 
 	 * 	returns:
 	 * 		-	The value from the GET or POST global array, if not exists, the value of the parameter return
 	 */
-	function requestParam($value,$return=NULL,$erase=false)
+	function requestParam($value,$return=NULL,$erase=false,$order=array("getParam","postParam"))
 	{
-		$ret = getParam($value,$return,$erase);	
-		if($ret === NULL) $ret = postParam($value,$return,$erase);
+		$ret = amslib::$order[0]($value,$return,$erase);	
+		if($ret === NULL) $ret = amslib::$order[1]($value,$return,$erase);
 		
 		return $ret;
-	}	
+	}
 }
 ?>
