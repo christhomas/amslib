930110254789bb96fa9d8b83e76f4795cf8693eb
Christopher Thomas <chris.thomas@antimatter-studios.com>
2010-06-24 18:20:33 +0200

    Amslib_Image changes.
    - add new methods to Amslib_Image for setting an error and MIME type of images
    - refactor the code in Amslib_Image so it's not all sitting in a single method
    - add methods for caching files processed do they don't have to be processed continually
    - add methods to find filenames, etc, based on the information given
    - rework the create method so that it can use the file cache in order to "create" a new image file handle
    - make all filenames passed through the object, into unique sha1 codes which represent the query that was made, this allows us to quickly find what options and what commands were asked, without having to pre-process all the information
    - add controls to width's and heights so they are integers and not partial floating point numbers
    - expand the crop method so it can accept a rectangle into both src+dest in order to crop out a section
    - refactor all the resizeTo.... methods so they use the scaleDimension function instead
    - add a way to obtain the dimensions of an image so scripts can process this custom information
    - add a cache method in order to create the file on disk, but not process it to a file or browser
    - split the write methods into separate paths for writing to browser and writing to disk, it makes the logic simpler and I dont think the extra codepaths cause any problems (perhaps we need to refactor again any common code)
    
    the rest of the changes seem to be just removing trailing space from the ends of lines.
    
    Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- Amslib_Image.php
+++ Amslib_Image.php
@@ -11,10 +11,53 @@ class Amslib_Image
 	const ERROR_WRITE_DIRECTORY_NOT_EXIST = "destination directory does not exist";
 	const ERROR_WRITE_FILE_EXIST = "file destination exists, delete original file first";
 
+	protected function setError($error)
+	{
+		if(is_string($error)){
+			$this->error = "ERROR: $error\n";
+		}
+	}
+
+	protected function setMIMEType($filename)
+	{
+		$extension = end(explode(".",$filename));
+
+		switch($extension){
+			case "jpeg":
+			case "jpg":{	return "image/jpeg";	}break;
+			case "png":{	return "image/png";		}break;
+			case "gif":{	return "image/gif";		}break;
+		}
+
+		return false;
+	}
+
+	protected function testWriteLocation($destination)
+	{
+		$directory = dirname($destination);
+
+		if(!is_dir($directory)){
+			$this->setError(self::ERROR_WRITE_DIRECTORY_NOT_EXIST);
+			return false;
+		}
+
+		if(file_exists($destination)){
+			$this->setError(self::ERROR_WRITE_FILE_EXIST);
+			return false;
+		}
+
+		return $destination;
+	}
+
+	protected function scaleDimension($d1,$d2,$d3)
+	{
+		return $d3 * ($d1 / $d2);
+	}
+
 	public function __construct()
 	{
 		if(!function_exists('imagecreatetruecolor')) {
-			$this->error(self::ERROR_PHPGD_NOT_FOUND);
+			$this->setError(self::ERROR_PHPGD_NOT_FOUND);
 		}
 
 		$this->cache		=	false;
@@ -22,65 +65,130 @@ class Amslib_Image
 		$this->allowedTypes	=	array("jpeg","jpg","gif","png");
 	}
 
-	public function error($error)
-	{
-		if(is_string($error)){
-			$this->error = "ERROR: $error\n";
-		}
-	}
-
 	public function enableCache($location)
 	{
-		$this->cache = $location;
+		$this->cache = $this->getAbsoluteLocation($location);
 	}
 
-	public function create($filename,$overwrite=false)
+	public function clearCache()
 	{
-		if($overwrite == false && isset($this->images[$filename])) return false;
+		$files = glob("$location/*");
 
-		$filename	=	Amslib_Filesystem::documentRoot().strtolower($filename);
-		$extension	=	end(explode(".",$filename));
+		foreach($files as $f){
+			//	Just in case someone managed to write a file with .. in it (back directory)
+			@unlink(str_replace("..","",$f));
+		}
+	}
 
-		if(in_array($extension,$this->allowedTypes) && file_exists($filename))
-		{
-			$image	=	false;
-			$mime	=	false;
+	public function getFromCache($parameters)
+	{
+		//	If caching is disabled, or the source file is not found, return false
+		if(!$this->cache) return false;
+		if(!isset($parameters["image"])) return false;
 
-			switch($extension){
-				case "jpeg":
-				case "jpg":{
-					$image	=	imagecreatefromjpeg($filename);
-					$mime	=	"image/jpeg";
-				}break;
+		$parameters["cache"]	=	true;
+		$filename				=	$this->create($parameters,false);
 
-				case "png":{
-					$image	=	imagecreatefrompng($filename);
-					$mime	=	"image/png";
-				}break;
+		//	check cache file exists
+		if($filename){
+			$data			=	$this->images[$filename];
+			$data["cache"]	=	true;
 
-				case "gif":{
-					$image	=	imagecreatefromgif($filename);
-					$mime	=	"image/gif";
-				}break;
-			}
-
-			if($image){
-				$this->images[$filename] = array(
-					"handle"	=>	$image,
-					"file"		=>	$filename,
-					"width"		=>	imagesx($image),
-					"height"	=>	imagesy($image),
-					"mime"		=>	$mime,
-				);
-
-				return $filename;
-			}else{
-				$this->error(self::ERROR_CREATE_IMAGE_OBJECT);
-			}
-		}else{
-			$this->error(self::ERROR_FILE_EXTENSION_INVALID);
+			return $filename;
 		}
 
+		//	The attempt to open the cache file failed, so close off any used resources
+		$this->close($filename);
+
+		return false;
+	}
+
+	public function getCacheFilename($filename)
+	{
+		return str_replace("//","/",$this->cache."/".$filename);
+	}
+
+	public function getAbsoluteLocation($filename)
+	{
+		$root		=	Amslib_Filesystem::documentRoot();
+		$filename	=	str_replace($root,"",$filename);
+		return str_replace("//","/","$root/$filename");
+	}
+
+	public function create($parameters,$overwrite=false)
+	{
+		//	Make the path absolute and obtain a unique name for this requested file
+		$parameters["image"]	=	$this->getAbsoluteLocation($parameters["image"]);
+		//	Get the extension for this file
+		$extension				=	end(explode(".",strtolower($parameters["image"])));
+		$uniqueName				=	sha1(http_build_query($parameters)).".$extension";
+
+		//	Obtain the cache name, or the disk filename
+		//	The cache name is the sha1 of the unique name, stored inside the cache directory
+		if(isset($parameters["cache"])){
+			$filename = $this->getCacheFilename($uniqueName);
+		}else{
+			$filename = $parameters["image"];
+		}
+
+		if(!file_exists($filename)) return false;
+
+		//	If the file exists and overwrite is false, you can't proceed
+		if($overwrite == false && isset($this->images[$filename])) return false;
+
+		//	Check the extension is valid and the file exists on disk
+		if(in_array($extension,$this->allowedTypes))
+		{
+			$handle = false;
+
+			//	Open and obtain a handle for the image
+			switch($extension){
+				case "jpeg":
+				case "jpg":{	$handle	=	imagecreatefromjpeg($filename);		}break;
+				case "png":{	$handle	=	imagecreatefrompng($filename);		}break;
+				case "gif":{	$handle	=	imagecreatefromgif($filename);		}break;
+			}
+
+			//	If valid, construct a structure to hold the data about the opened file
+			if($handle){
+				$this->images[$uniqueName] = array(
+					"handle"	=>	$handle,
+					"file"		=>	$parameters["image"],
+					"width"		=>	imagesx($handle),
+					"height"	=>	imagesy($handle),
+					"mime"		=>	$this->setMIMEType($filename),
+					"cache"		=>	false
+				);
+
+				return $uniqueName;
+			}else{
+				$this->setError(self::ERROR_CREATE_IMAGE_OBJECT);
+			}
+		}else{
+			$this->setError(self::ERROR_FILE_EXTENSION_INVALID);
+		}
+
+		return false;
+	}
+
+	public function maxdim($filename,$width,$height)
+	{
+		$d = $this->getDimensions($filename);
+
+		if($d){
+			if($d["width"] > $width){
+				$ratio			=	$width / $d["width"];
+				$d["width"]		*=	$ratio;
+				$d["height"]	*=	$ratio;
+			}
+
+			if($d["height"] > $height){
+				$ratio			=	$height / $d["height"];
+				$d["width"]		*=	$ratio;
+				$d["height"]	*=	$ratio;
+			}
+			return $this->resize($filename,$d["width"],$d["height"]);
+		}
 		return false;
 	}
 
@@ -88,12 +196,13 @@ class Amslib_Image
 	{
 		if(!isset($this->images[$filename])) return false;
 
-		$image = $this->images[$filename];
-
-		$tmp_image = imagecreatetruecolor($width, $height);
+		$width		=	(int)$width;
+		$height		=	(int)$height;
+		$image		=	$this->images[$filename];
+		$tmp_image	=	imagecreatetruecolor($width,$height);
 
 		if($tmp_image){
-			imagecopyresampled(
+			$success = imagecopyresampled(
 				$tmp_image, $image["handle"],
 				0, 0,
 				0, 0,
@@ -101,45 +210,58 @@ class Amslib_Image
 				$image["width"],$image["height"]
 			);
 
-			$image["handle"]	=	$tmp_image;
-			$image["width"]		=	$width;
-			$image["height"]	=	$height;
+			if($success){
+				$this->close($filename);
+				$image["handle"]	=	$tmp_image;
+				$image["width"]		=	$width;
+				$image["height"]	=	$height;
+				$this->images[$filename] = $image;
 
-			$filename = "{$image["file"]}?width=$width&height=$height";
-			unset($this->images[$filename]);
-			$this->images[$filename] = $image;
-		}else $this->error(self::ERROR_CREATE_IMAGE_OBJECT);
+				return $filename;
+			}
+		}
+
+		$this->setError(self::ERROR_CREATE_IMAGE_OBJECT);
 
 		return $filename;
 	}
 
-	public function crop($filename,$x,$y,$width,$height)
+	public function crop($filename,$sx,$sy,$dx,$dy,$sw,$sh,$dw,$dh)
 	{
 		if(!isset($this->images[$filename])) return false;
 
-		$image = $this->image[$filename];
-
-		$tmp_image = imagecreatetruecolor($width,$height);
+		$sx			=	(int)$sx;
+		$sy			=	(int)$sy;
+		$dx			=	(int)$dx;
+		$dy			=	(int)$dy;
+		$sw			=	(int)$sw;
+		$sh			=	(int)$sh;
+		$dw			=	(int)$dw;
+		$dh			=	(int)$dh;
+		$image		=	$this->images[$filename];
+		$tmp_image	=	imagecreatetruecolor($dw,$dh);
 
 		if($tmp_image){
-			imagecopyresampled(
+			$success = imagecopyresampled(
 				$tmp_image,$image["handle"],
-				0, 0,
-				$x, $y,
-				$width, $height,
-				$width, $height
+				$dx,$dy,$sx,$sy,
+				$dw,$dh,$sw,$sh
 			);
 
-			$image["handle"]	=	$tmp_image;
-			$image["width"]		=	$width;
-			$image["height"]	=	$height;
+			if($success){
+				$this->close($filename);
+				$image["handle"]	=	$tmp_image;
+				$image["width"]		=	$dw;
+				$image["height"]	=	$dh;
+				$this->images[$filename] = $image;
 
-			$filename = "{$image["file"]}?width=$width&height=$height";
-			unset($this->images[$filename]);
-			$this->images[$filename] = $image;
-		}else $this->error(self::ERROR_CREATE_IMAGE_OBJECT);
+				return $filename;
+			}
+		}
 
-		return $filename;
+		$this->setError(self::ERROR_CREATE_IMAGE_OBJECT);
+
+		return false;
 	}
 
 	public function resizeToWidth($filename,$width)
@@ -147,9 +269,7 @@ class Amslib_Image
 		if(!isset($this->images[$filename])) return false;
 
 		$image	=	$this->images[$filename];
-
-		$ratio	=	$width / $image["width"];
-		$height	=	$image["height"] * $ratio;
+		$height	=	$this->scaleDimension($width,$image["width"],$image["height"]);
 
 		return $this->resize($filename,$width,$height);
 	}
@@ -159,9 +279,7 @@ class Amslib_Image
 		if(!isset($this->images[$filename])) return false;
 
 		$image	=	$this->images[$filename];
-
-		$ratio	=	$height / $image["height"];
-		$width	=	$image["width"] * $ratio;
+		$width	=	$this->scaleDimension($height,$image["height"],$image["width"]);
 
 		return $this->resize($filename,$width,$height);
 	}
@@ -178,59 +296,152 @@ class Amslib_Image
 		return $this->resize($width,$height);
 	}
 
-	public function writeFile($filename,$destination=NULL)
+	public function getDimensions($filename)
+	{
+		if(!isset($this->images[$filename])) return false;
+
+		return array(
+			"width"		=>	$this->images[$filename]["width"],
+			"height"	=>	$this->images[$filename]["height"]
+		);
+	}
+
+	public function cache($filename)
+	{
+		if(!$this->cache)						return false;
+		if(!isset($this->images[$filename]))	return false;
+
+		$image = $this->images[$filename];
+
+		//	File is cached, just read it out and return
+		if($image["cache"]) return true;
+
+		//	Grab the extension and test whether it's valid or not
+		$extension = end(explode(".",$image["file"]));
+		if(in_array($extension,$this->allowedTypes))
+		{
+			//	If cache is enabled, set the output to be a file
+			$destination = $this->getCacheFilename($filename);
+
+			switch($extension){
+				case "jpeg":
+				case "jpg":{	imagejpeg($image["handle"],$destination);		}break;
+				case "png":{	imagepng($image["handle"],$destination);		}break;
+				case "gif":{	imagegif($image["handle"],$destination);		}break;
+			};
+
+			return true;
+		}
+
+		$this->setError(self::ERROR_FILE_EXTENSION_INVALID);
+
+		return false;
+	}
+
+	public function write($filename)
 	{
 		if(!isset($this->images[$filename])) return false;
 
 		$image = $this->images[$filename];
 
-		if($destination !== NULL){
-			$directory = dirname($destination);
+		//	File is cached, just read it out and return
+		if($image["cache"]){
+			readfile($filename);
 
-			if(!is_dir($directory)){
-				$this->error(self::ERROR_WRITE_DIRECTORY_NOT_EXIST);
-				return false;
-			}
-
-			if(file_exists($destination)){
-				$this->error(self::ERROR_WRITE_FILE_EXIST);
-				return false;
-			}
-
-			$fn = $destination;
-		}else{
-			$fn = $image["file"];
+			return true;
 		}
 
-		$extension = end(explode(".",$fn));
+		//	Grab the extension and test whether it's valid or not
+		$extension = end(explode(".",$image["file"]));
+		if(in_array($extension,$this->allowedTypes))
+		{
+			//	If cache is enabled, set the output to be a file
+			$destination = ($this->cache) ? $this->getCacheFilename($filename) : NULL;
 
-		if(in_array($extension,$this->allowedTypes)){
 			switch($extension){
 				case "jpeg":
-				case "jpg":{
-					imagejpeg($image["handle"],$destination);
-				}break;
+				case "jpg":{	imagejpeg($image["handle"],$destination);		}break;
+				case "png":{	imagepng($image["handle"],$destination);		}break;
+				case "gif":{	imagegif($image["handle"],$destination);		}break;
+			};
 
-				case "png":{
-					imagepng($image["handle"],$destination);
-				}break;
-
-				case "gif":{
-					imagegif($image["handle"],$destination);
-				}break;
+			//	If cache is enabled, you have to output a copy now to the browser
+			if($this->cache){
+				chmod($destination,0777);
+				readfile($destination);
 			}
+
+			return true;
 		}
 
-		$this->error(self::ERROR_FILE_EXTENSION_INVALID);
+		$this->setError(self::ERROR_FILE_EXTENSION_INVALID);
 
 		return false;
 	}
 
-	public function getMime($filename)
+	public function writeToDisk($filename,$destination)
 	{
 		if(!isset($this->images[$filename])) return false;
 
-		return $this->image[$filename]["mime"];
+		$image = $this->images[$filename];
+
+		$destination = $this->testWriteLocation($destination);
+		if(!$destination) return false;
+
+		//	Image is cached, just copy it to the new location
+		if($image["cache"]){
+			copy($filename,$destination);
+			chmod($destination,0777);
+
+			return true;
+		}
+
+		//	Grab the extension and test whether it's valid or not
+		$extension = end(explode(".",$destination));
+		if(in_array($extension,$this->allowedTypes)){
+			switch($extension){
+				case "jpeg":
+				case "jpg":{	imagejpeg($image["handle"],$destination);		}break;
+				case "png":{	imagepng($image["handle"],$destination);		}break;
+				case "gif":{	imagegif($image["handle"],$destination);		}break;
+			};
+
+			//	Write the file to a new destination, or to the browser
+			if(file_exists($destination)){
+				chmod($destination,0777);
+
+				//	Write a copy of this file into the cache
+				if($this->cache){
+					$cacheName = $this->getCacheFilename($filename,$extension);
+					copy($destination,$cacheName);
+					chmod($cacheName,0777);
+				}
+
+				return true;
+			}
+		}
+
+		$this->setError(self::ERROR_FILE_EXTENSION_INVALID);
+
+		return false;
+	}
+
+	public function close($filename=NULL)
+	{
+		if(isset($this->images[$filename])){
+			imagedestroy($this->images[$filename]["handle"]);
+		}else{
+			foreach($this->images as $i){
+				imagedestroy($i["handle"]);
+			}
+		}
+	}
+
+	public function getMIMEType($filename)
+	{
+		if(!isset($this->images[$filename])) return false;
+
+		return $this->images[$filename]["mime"];
 	}
 
 	public function &getInstance()
--- Amslib_WidgetManager.php
+++ Amslib_WidgetManager.php
@@ -1,11 +1,11 @@
 <?php
 /*******************************************************************************
- * Copyright (c) {15/03/2008} {Christopher Thomas} 
+ * Copyright (c) {15/03/2008} {Christopher Thomas}
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -14,12 +14,12 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ *
  * File: Amslib_WidgetManager.php
  * Title: Widget manager for component based development
- * Version: 2.2
+ * Version: 2.3
  * Project: amslib
- * 
+ *
  * Contributors/Author:
  *    {Christopher Thomas} - Creator - chris.thomas@antimatter-studios.com
  *******************************************************************************/
@@ -31,26 +31,26 @@ class Amslib_WidgetManager
  *******************************************************************************/
 	protected $xdoc;
 	protected $xpath;
-	
+
 	protected $documentRoot;
 	protected $widgetPath;
 	protected $websitePath;
-	
+
 	protected $api;
 	protected $stylesheet;
 	protected $javascript;
-		
+
 	protected function setAPI($name)
 	{
 		$list = $this->xpath->query("//package/object/api");
-		
+
 		$api = false;
 
 		if($list->length > 0){
 			$node = $list->item(0);
 			if($node){
 				Amslib::requireFile($this->widgetPath."/$name/objects/{$node->nodeValue}.php");
-				
+
 				if(method_exists($node->nodeValue,"getInstance")){
 					$api = call_user_func(array($node->nodeValue,"getInstance"));
 				}else{
@@ -58,27 +58,27 @@ class Amslib_WidgetManager
 				}
 			}
 		}
-		
-		//	Create a default MVC object			
+
+		//	Create a default MVC object
 		if($api == false) $api = new Amslib_MVC();
-		
+
 		$this->api[$name] = $api;
-		
+
 		//	Set the widget manager parent
 		$api->setWidgetManager($this);
 		$api->setWidgetName($name);
-		
+
 		return $this->api[$name];
 	}
-	
+
 	protected function preparePath($path)
 	{
 		$path = "{$path}__END__";
 		//	Make sure the path doesnt end with a trailing slash
 		$path = str_replace("/__END__","",$path);
-		//	Cleanup after the attempt to detect trailing slash	
+		//	Cleanup after the attempt to detect trailing slash
 		$path = str_replace("__END__","",$path);
-		
+
 		return $path;
 	}
 
@@ -86,80 +86,80 @@ class Amslib_WidgetManager
 	{
 		$xml = "$path/$name/package.xml";
 		if($p = Amslib::findPath($xml)) $xml = "$p/$xml";
-		
+
 		$this->xdoc = new DOMDocument('1.0', 'UTF-8');
 		if(@$this->xdoc->load($xml)){
 			$this->xdoc->preserveWhiteSpace = false;
 			$this->xpath = new DOMXPath($this->xdoc);
-			
+
 			return true;
 		}else{
 			print("XML FILE FAILED TO OPEN: path[$path], name[$name], xml[$xml]<br/>");
-			
+
 			return false;
 		}
 	}
-	
+
 	protected function findResource($widget,$node)
 	{
 		if($node->getAttribute("remote")) return $node->nodeValue;
-		
+
 		$file = $node->nodeValue;
 		$path = false;
-		
+
 		//	First try to find the file inside the widget itself
 		$wpath = str_replace("//","/","{$this->widgetPath}/$widget/$file");
 		//	if you found it inside the widget, use that version over any other
 		if(file_exists($wpath)) $path = $wpath;
-		
+
 		//	If you didn't find it, search the path
 		if($path == false){
 			//	can't find the widget, search the path instead
 			$fpath = Amslib::findPath($file);
-			
+
 			//	if you found it, assign it
 			if($fpath) $path = "$fpath/$file";
 		}
-		
+
 		//	relativise the path and reduce the double slashes to single ones.
 		return str_replace("//","/",$this->getRelativePath($path));
 	}
-	
+
 	protected function loadConfiguration($path,$widget)
 	{
 		$api = $this->setAPI($widget);
 		$api->setPath($path);
-		
+
 		$controllers = $this->xpath->query("//package/controllers/name");
 		for($a=0;$a<$controllers->length;$a++){
 			$name = $controllers->item($a)->nodeValue;
 			$api->setController($name);
 		}
-		
+
 		$layouts = $this->xpath->query("//package/layout/name");
 		for($a=0;$a<$layouts->length;$a++){
 			$name = $layouts->item($a)->nodeValue;
 			$api->setLayout($name);
 		}
-		
+
 		$views = $this->xpath->query("//package/view/name");
 		for($a=0;$a<$views->length;$a++){
 			$name = $views->item($a)->nodeValue;
 			$api->setView($name);
 		}
-		
+
 		$objects = $this->xpath->query("//package/object/name");
 		for($a=0;$a<$objects->length;$a++){
 			$name = $objects->item($a)->nodeValue;
 			$api->setObject($name);
 		}
-		
+
 		$theme = $this->xpath->query("//package/theme/name");
 		for($a=0;$a<$theme->length;$a++){
 			$name = $theme->item($a)->nodeValue;
 			$api->setTheme($name);
 		}
-		
+
 		//	FIXME: why are services treated differently then other parts of the MVC system?
 		$services = $this->xpath->query("//package/service/file");
 		for($a=0;$a<$services->length;$a++){
@@ -168,21 +168,21 @@ class Amslib_WidgetManager
 			$file = $this->getRelativePath($this->widgetPath."/$widget/services/$file");
 			$api->setService($name,$file);
 		}
-		
+
 		$images = $this->xpath->query("//package/image/file");
 		for($a=0;$a<$images->length;$a++){
 			$name = $images->item($a)->getAttribute("name");
 			$file = $this->findResource($widget,$images->item($a));
 			$api->setImage($name,$file);
 		}
-		
+
 		$javascript = $this->xpath->query("//package/javascript/file");
 		for($a=0;$a<$javascript->length;$a++){
 			$name = $javascript->item($a)->getAttribute("name");
 			$file = $this->findResource($widget,$javascript->item($a));
 			$this->setJavascript($name,$file);
 		}
-		
+
 		$stylesheet = $this->xpath->query("//package/stylesheet/file");
 		for($a=0;$a<$stylesheet->length;$a++){
 			$name = $stylesheet->item($a)->getAttribute("name");
@@ -190,65 +190,65 @@ class Amslib_WidgetManager
 			$this->setStylesheet($name,$file);
 		}
 	}
-	
+
 /*******************************************************************************
  *	PUBLIC METHODS
  *******************************************************************************/
 	public function __construct()
 	{
 		//	Setup the basic system like this, it's 99% correct everytime
-		$this->setup("{$_SERVER["DOCUMENT_ROOT"]}/widgets",$_SERVER["DOCUMENT_ROOT"]);
-		
+		$this->setup(Amslib_Filesystem::documentRoot()."/widgets",Amslib_Filesystem::documentRoot());
+
 		$this->stylesheet	=	array();
 		$this->javascript	=	array();
 	}
-	
+
 	public function &getInstance()
 	{
 		static $instance = NULL;
-		
-		if($instance === NULL) $instance = new Amslib_WidgetManager();	
-		
+
+		if($instance === NULL) $instance = new Amslib_WidgetManager();
+
 		return $instance;
 	}
-	
+
 	//	initialise all the required basic information
 	public function setup($widgetPath,$websitePath=NULL)
 	{
 		//	Make sure the website path is not null, default to the server root (99% correct everytime)
-		if($websitePath == NULL) $websitePath = $_SERVER["DOCUMENT_ROOT"];
-		
-		$this->documentRoot =	$this->preparePath($_SERVER["DOCUMENT_ROOT"]);
+		if($websitePath == NULL) $websitePath = Amslib_Filesystem::documentRoot();
+
+		$this->documentRoot =	$this->preparePath(Amslib_Filesystem::documentRoot());
 		$this->widgetPath	=	$this->preparePath($widgetPath);
 		$this->websitePath	=	$this->preparePath($websitePath);
-				
+
 		//	Make website path into an absolute path
 		$path = $this->documentRoot.$this->websitePath;
 		$path = str_replace($this->documentRoot,"",$path);
 		$this->websitePath = $this->documentRoot.$path;
-		
+
 		//	Make widgetPath into an absolute path
 		$path = $this->documentRoot.$this->widgetPath;
 		$path = str_replace($this->documentRoot,"",$path);
 		$this->widgetPath = $this->documentRoot.$path;
 	}
-	
+
 	public function getAPI($name)
 	{
 		return $this->api[$name];
 	}
-	
+
 	/**
 	 * method: overrideAPI
-	 * 
+	 *
 	 * A way to provide a custom API object that deals with your application specific
 	 * layer and perhaps provides a customised way to deal with the widget in question
 	 * and your qpplication.
-	 * 
+	 *
 	 * parameters:
 	 * 	$name	-	The name of the widget being overriden
 	 * 	$api	-	An object which is to be used to override the default widget api
-	 * 
+	 *
 	 * example:
 	 *		require_once("CustomApp_Amstudios_Message_List.php");
 	 *		class CustomApp_Amstudios_Message_List extends Amstudios_Message_List{}
@@ -261,7 +261,7 @@ class Amslib_WidgetManager
 	{
 		$this->api[$name] = $api;
 	}
-	
+
 	public function load($name)
 	{
 		if(is_array($name)){
@@ -274,14 +274,14 @@ class Amslib_WidgetManager
 			}
 		}
 	}
-	
+
 	public function getWidgetPath()
 	{
 		return $this->widgetPath;
 	}
-	
+
 	public function getRelativePath($path="")
-	{	
+	{
 		//	Path is already relative
 		if(strpos($path,".") === 0) return $path;
 
@@ -290,36 +290,36 @@ class Amslib_WidgetManager
 
 		return $path;
 	}
-	
-	
+
+
 	public function setStylesheet($name,$file)
 	{
 		if($name && $file){
 			$this->stylesheet[$name] = "<link rel='stylesheet' type='text/css' href='$file' />";
 		}
 	}
-	
+
 	public function getStylesheet()
 	{
 		return implode("\n",$this->stylesheet);
 	}
-	
+
 	public function setJavascript($name,$file)
 	{
 		if($name && $file){
 			$this->javascript[$name] = "<script type='text/javascript' src='$file'></script>";
 		}
 	}
-	
+
 	public function getJavascript()
 	{
 		return implode("\n",$this->javascript);
 	}
-	
+
 	public function render($name,$parameters=array())
 	{
 		$api = $this->getAPI($name);
-		
+
 		return ($api) ? $api->render($parameters) : false;
 	}
 }
