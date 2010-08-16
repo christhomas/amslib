71875afd151e5cb1485435f3246911e55be8272f
Christopher Thomas <chris.thomas@antimatter-studios.com>
2010-08-16 13:27:10 +0200

    extend the functionality of this javascript class to include - ability to register a callback and obtain a callback generically without dealing with customised code - ability to read translations from the php side of the code where translations cannot be imported
    Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- javascript/Amslib_Widget.js
+++ javascript/Amslib_Widget.js
@@ -1,13 +1,17 @@
 var Amslib_Widget = Class.create(
 {
-	mainWidget: false,
-	value:		false,
-	services:	false,
+	mainWidget: 	false,
+	value:			false,
+	services:		false,
+	translation:	false,
+	callback:		false,
 	
 	initialize: function(selector)
 	{
-		this.value		=	new Hash();
-		this.services	=	new Hash();
+		this.value			=	new Hash();
+		this.services		=	new Hash();
+		this.callback		=	new Hash();
+		this.translation	=	new Hash();
 		
 		//	Try first to just pass it through prototype, then if fails, use as a selector
 		this.mainWidget = $(selector);
@@ -18,6 +22,32 @@ var Amslib_Widget = Class.create(
 		
 		return this;
 	},
+	
+	getNode: function()
+	{
+		return this.mainWidget;
+	},
+	
+	identify: function()
+	{
+		return this.mainWidget.identify();
+	},
+	
+	setCallback: function(type,callback)
+	{
+		this.callback.set(type,callback);
+	},
+	
+	defaultCallback: function(){
+		if(console && console.log) console.log("DEFAULT CALLBACK CALLED");
+	},
+	
+	getCallback: function(type)
+	{
+		var cb = this.callback.get(type);
+				
+		return (cb) ? cb : this.defaultCallback;
+	},
 
 	readParameters: function()
 	{
@@ -26,6 +56,8 @@ var Amslib_Widget = Class.create(
 		this.mainWidget.select(".widget_parameters input[type='hidden']").each(function(p){
 			if(p.name.indexOf("service:") >= 0){
 				this.setService(p.name.replace("service:",""),p.value);
+			}else if(p.name.indexOf("translation:") >= 0){
+				this.setTranslation(p.name.replace("translation:",""),p.value);
 			}else{
 				this.setValue(p.name,p.value);
 			}
@@ -50,5 +82,15 @@ var Amslib_Widget = Class.create(
 	getService: function(name)
 	{
 		return this.services.get(name);
+	},
+	
+	setTranslation: function(name,value)
+	{
+		this.translation.set(name,value);
+	},
+	
+	getTranslation: function(name)
+	{
+		return this.translation.get(name);
 	}
 });
