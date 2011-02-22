7efba3aa57515a3238b971405a0412320232d7da
Christopher Thomas <chris.thomas@antimatter-studios.com>
2011-02-22 14:52:20 +0100

    comment out the getInstance method because I believe it was allowing incorrectly programmed child classes to function, but then crap out, I would prefer they just fatal error and make the programmer deal with it
    Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- database/Amslib_Database_MySQL.php
+++ database/Amslib_Database_MySQL.php
@@ -309,14 +309,15 @@ HAS_TABLE;
 	{
 		return mysql_error();
 	}
-
-	public function &getInstance($connect=true)
+	
+	//	NOTE: I think this method is a bad idea, so I'm commenting it out to see what happens
+	/*public function &getInstance($connect=true)
 	{
 		static $instance = NULL;
 
-		if($instance === NULL) $instance = new Amslib_Database_MySQL($connect);
+		if($instance === NULL) $instance = new self($connect);
 
 		return $instance;
-	}
+	}*/
 }
 ?>
