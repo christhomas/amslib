bf438d65afa6e69504cad0e5e66ed8d110ff8fbe
Christopher Thomas <chris.thomas@antimatter-studios.com>
2011-02-27 21:24:53 +0100

    strip all the trailing whitespace from the entire file
    Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- router/Amslib_Router3.php
+++ router/Amslib_Router3.php
@@ -33,9 +33,9 @@ class Amslib_Router3
 
 	/**
 	 * method: sanitise
-	 * 
+	 *
 	 * If the route was invalid, set a false "fake" array of data
-	 * to stop you from having to do all kinds of crazy test by default 
+	 * to stop you from having to do all kinds of crazy test by default
 	 * in other methods
 	 */
 	static protected function sanitise($route)
@@ -49,21 +49,21 @@ class Amslib_Router3
 			$route["parameters"]	=	array();
 			$route["options"]		=	array();
 		}
-		
+
 		return $route;
 	}
-	
+
 	public function __construct()
 	{
 		//	IMPORTANT: This method might be full of information which doesnt need to be here!!!
 		//	TODO: Analyse who uses the information set here to determine whether it's safe to "move" this somewhere else.
-		
+
 		self::$location	=	Amslib_Website::set();
-		
+
 		//	Setup the router path in order to obtain the correct path information
 		$router_path	=	Amslib::getParam("router_path");
 		self::$path		=	NULL;
-		
+
 		if($router_path){
 			//	This is so ugly but I can't think of another way to do it.
 			//	First strip all the // -> /, then strip self::$location, then strip // -> from /self::$location/
@@ -74,39 +74,39 @@ class Amslib_Router3
 			self::$path = str_replace("//","/","/".self::$path."/");
 		}
 	}
-	
+
 	static public function setSource($source)
 	{
 		self::$source = $source;
 	}
-	
+
 	/**
 	 * method:	getObject
-	 * 
+	 *
 	 * It's hard to know when a router source object is compatible or not
 	 * this method eliminates that doubt by putting the onus on this object
-	 * to create the appropriate object for you, depending on the type you 
+	 * to create the appropriate object for you, depending on the type you
 	 * requested.
 	 */
 	static public function getObject($type,$file=NULL)
 	{
 		switch($type){
-			case "source:xml":{	
-				return Amslib_Router_Source_XML2::getInstance();	
+			case "source:xml":{
+				return Amslib_Router_Source_XML2::getInstance();
 			}break;
-			
-			case "source:database":{		
-				return Amslib_Router_Source_Database::getInstance();	
+
+			case "source:database":{
+				return Amslib_Router_Source_Database::getInstance();
 			}break;
-			
+
 			case "language":{
 				return Amslib_Router_Language3::getInstance();
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	static public function execute()
 	{
 		if(self::$source && self::$path){
@@ -114,12 +114,12 @@ class Amslib_Router3
 			self::$route	=	self::getRouteByURL(self::$path);
 		}
 	}
-	
+
 	/**
 	 * method: getURL
-	 * 
+	 *
 	 * Return a url based on the name,version,lang being requested
-	 * 
+	 *
 	 * parameters:
 	 * 	$name		-	The name of the route, defined in each route declaration
 	 * 	$version	-	The version of the route (not 100% sure what this is for anymore, old code?)
@@ -128,70 +128,70 @@ class Amslib_Router3
 	static public function getURL($name=NULL,$version=NULL,$lang=NULL)
 	{
 		$route = self::getRoute($name,$version,$lang);
-		
+
 		//	TODO: Support passing the $lang parameter and have it override the language setup
 		$lang = Amslib_Router_Language3::getName();
 		if($lang) $lang = "/$lang/";
-		
+
 		return Amslib_Website::rel($lang.$route["route"]);
 	}
-	
+
 	static public function getRoute($name=NULL,$version=NULL,$lang=NULL)
 	{
 		if($name == NULL) return self::$route;
 
 		if($lang == NULL) $lang = Amslib_Router_Language3::getCode();
-		
+
 		return self::sanitise(self::$source->getRoute($name,$version,$lang));
 	}
-	
+
 	static public function getRouteByURL($url=NULL)
 	{
 		if($url == NULL) return self::$route;
-		
+
 		return self::sanitise(self::$source->getRouteByURL($url));
 	}
-	
+
 	static public function getName()
 	{
 		return self::$route["name"];
 	}
-	
+
 	static public function isRouted()
 	{
 		return self::path !== NULL ? true : false;
 	}
-	
+
 	static public function getResource($name=NULL)
 	{
 		$r = ($name == NULL) ? self::$route : self::getRoute($name);
-		
+
 		return $r["resource"];
 	}
-	
+
 	//	DEPRECATED USE getParameter instead (so I can return one parameter, or all of them at once)
 	static public function getParameters(){ return self::getParameter(); }
-	
+
 	static public function getParameter($name=NULL)
 	{
-		return $name && isset(self::$route["parameters"][$name]) 
-			? self::$route["parameters"][$name] 
+		return $name && isset(self::$route["parameters"][$name])
+			? self::$route["parameters"][$name]
 			: self::$route["parameters"];
 	}
-	
+
 	static public function hasParameter($name)
 	{
 		return in_array($name,self::$route["parameters"]) ? true : false;
 	}
-	
+
 	static public function getURLOptions()
 	{
 		return self::$route["options"];
 	}
-		
+
 	/**
 	 * method: changeLang
-	 * 
+	 *
 	 * Create a url for the current route that will change the language into the other one
 	 * this is completely application specific in how this happens, per application, requires perhaps
 	 * a different way to change languages, as long as the url scheme for languages remains the same
@@ -200,11 +200,11 @@ class Amslib_Router3
 	{
 		Amslib_Router_Language3::push();
 		Amslib_Router_Language3::set($langName);
-		
+
 		$u = self::getURL();
-		
+
 		Amslib_Router_Language3::pop();
-		
+
 		return $u;
 	}
 
