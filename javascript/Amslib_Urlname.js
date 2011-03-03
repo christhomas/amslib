a64dcbf0620b753bbf70cfb2efb1904f4d2beeda
Christopher Thomas <chris.thomas@antimatter-studios.com>
2011-03-03 12:47:34 +0100

    update the urlname code to be more logically structured and add an autoloader capability to it

--- javascript/Amslib_Urlname.js
+++ javascript/Amslib_Urlname.js
@@ -1,21 +1,50 @@
 Amslib_Urlname = Class.create(
 {
+	src:	false,
+	dest:	false,
+	
 	initialize: function(src,dest)
 	{
-		src.observe("keyup",function(event){
-			dest.value = this.slugify(src.value);
-		}.bind(this));
+		this.src	=	src;
+		this.dest	=	dest;
 		
-		src.observe("change",function(event){
-			dest.value = this.slugify(src.value);
-		}.bind(this));
+		this.src.observe("keyup",this.updateFromSrc.bind(this));
+		this.src.observe("change",this.updateFromSrc.bind(this));
+		this.dest.observe("keyup",this.updateFromDest.bind(this));
 		
-		dest.observe("keyup",function(event){
-			dest.value = this.slugify(dest.value);
-		}.bind(this));
+		//	Initialise any empty values if there is
+		//	a) nothing already set
+		//	b) there is something to update from
+		if(src.value.length > 0 && dest.value.length == 0){
+			this.updateFromSrc();
+		}
+	},
+	
+	updateFromSrc: function()
+	{
+		this.dest.value = this.slugify(this.src.value);
+	},
+	
+	updateFromDest: function()
+	{
+		this.dest.value = this.slugify(this.dest.value);
 	},
 
 	slugify: function(text){
 		return text.replace(/[^-a-zA-Z0-9]+/ig, '-').toLowerCase();
 	}
 });
+
+Amslib_Urlname.autoload = function()
+{
+	$$(".amslib_urlname_parent.autoload").each(function(urlname){
+		var src = urlname.down(".amslib_urlname_src");
+		var dst = urlname.down(".amslib_urlname_dst");
+		
+		if(src && dst){
+			src.store("amslib_urlname",new Amslib_Urlname(src,dst));
+		}
+	});
+}
+
+Event.observe(window,"load",Amslib_Urlname.autoload);
