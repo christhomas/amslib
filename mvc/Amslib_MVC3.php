770aec5f98d40e50d1d9fc4c0aa23661492c6ae7
Christopher Thomas <chris.thomas@antimatter-studios.com>
2011-04-14 20:13:43 +0200

    -	add a url (boolean) parameter to getService which lets you return the url instead of the service php script if required (in the html, javascript you normally use the url's for the services, not the actual php script) and in fact, the url's will probably be mandatory in future and the reversal of the situation will be done (to get the php script, you'll need to call a specific out of the way function instead and the default will be to return the url -	add a method which does the same as the extra parameter in the getService method but does it without any need for an extra parameter, so it's a bit cleaner.Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- mvc/Amslib_MVC3.php
+++ mvc/Amslib_MVC3.php
@@ -271,9 +271,16 @@ class Amslib_MVC3
 		}
 	}
 
-	public function getService($id)
+	public function getService($id,$url=false)
 	{
+		if($url) return $this->getValue("service:$id");
+		
 		return (isset($this->service[$id])) ? $this->service[$id] : NULL;
+	}
+	
+	public function getServiceURL($id)
+	{
+		return $this->getValue("service:$id");
 	}
 
 	public function callService($id)
