6878db59786d5e80c0810351f0312ce74bb8f209
Christopher Thomas <chris.thomas@antimatter-studios.com>
2011-08-19 11:33:02 +0200

    -	update the accordion to a class instead of a function, means I can do more things in the future to add extra features, etc -	update the service code to support ajax calls -	update to version 7.1Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- js/Amslib_Accordion.js
+++ js/Amslib_Accordion.js
@@ -1,16 +1,41 @@
-//	Well.......that was easy....erm...should I make this a class? 
-$(document).ready(function(){
-	var src=".amslib_accordion_trigger",dst=".amslib_accordion_target",o="open",time=1000;
-	$(src).bind("click",function(){
-		var t = $(this).next(dst);
-		$(src).removeClass(o);
+var Amslib_Accordion = my.Amslib_Accordion = my.Class(
+{
+	STATIC: {
+		autoload: function(){
+			$(Amslib_Accordion.options.src).each(function(){
+				new Amslib_Accordion($(this));
+			})
+		},
 		
-		if(t.hasClass(o)){
-			t.slideUp().removeClass(o);
-		}else{
-			$(this).siblings(".amslib_accordion_target."+o).slideUp().removeClass(o);
-			$(this).addClass(o);
-			t.slideDown(time).addClass(o);
+		options: {
+			src: ".amslib_accordion_trigger",
+			dst: ".amslib_accordion_target",
+			open: "open",
+			time: 1000
 		}
-	});
+	},
+	
+	constructor: function(parent)
+	{
+		var p = $(parent);
+		var d = Amslib_Accordion.options.dst;
+		var t = Amslib_Accordion.options.time;
+		var o = Amslib_Accordion.options.open;
+		
+		p.bind("click",function(){
+			l = p.next(d);
+			p.removeClass(o);
+			
+			if(l.hasClass(o)){
+				l.slideUp().removeClass(o);
+			}else{
+				p.siblings(d+"."+o).slideUp().removeClass(o);
+				p.addClass(o);
+				l.slideDown(t).addClass(o);
+			}
+		});
+	}
 });
+
+$(document).ready(Amslib_Accordion.autoload);
+ 
