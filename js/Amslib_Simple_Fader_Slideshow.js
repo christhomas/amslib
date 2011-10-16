48b0055c90b552221f88e0911a84fc262453fab6
Christopher Thomas <chris.thomas@antimatter-studios.com>
2011-10-16 19:27:50 +0200

    remove the ckeditor source and sample directory in order to strip it down from using so many files.Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- js/Amslib_Simple_Fader_Slideshow.js
+++ js/Amslib_Simple_Fader_Slideshow.js
@@ -9,25 +9,32 @@ var Amslib_Simple_Fader_Slideshow = my.Amslib_Simple_Fader_Slideshow = my.Class(
 		//		NOTE:	There was another system employed in another project which let you "register" a callback
 		//		NOTE:	We need to load this callback and let it call the autoloader
 		autoload: function(){
-			$(".amslib_simple_fader_slideshow.amslib_autoload").each(function(){
+			$(Amslib_Simple_Fader_Slideshow.config.pselector+".amslib_autoload").each(function(){
 				new Amslib_Simple_Fader_Slideshow(this);
 			});
+		},
+		
+		config: {
+			pselector: ".amslib_simple_fader_slideshow",
+			cselector: "img"
 		}
 	},
 	
 	constructor: function(parent)
 	{
 		Amslib_Simple_Fader_Slideshow.Super.call(this,parent,"Amslib_Simple_Fader_Slideshow");
+		
+		var selector = Amslib_Simple_Fader_Slideshow.config.cselector;
 			
-		this.images		=	$("img",this.parent);
+		this.images		=	$(selector,this.parent);
 		this.timeout	=	$("input['amslib_simple_fader_slideshow_timeout']",this.parent).val();
 
 		//	Default a missing timeout to 5 seconds
 		if(!this.timeout) this.timeout = 5000;
 		
 		//	Make sure one of the images is set to active, if not, set it to the first image
-		var active = $("img.active",this.parent);
-		if(!active.length) $("img:first",this.parent).addClass("active");
+		var active = $(selector+".active",this.parent);
+		if(!active.length) $(selector+":first",this.parent).addClass("active");
 		
 		if(this.images.length > 1) this.start();
 	},
@@ -52,10 +59,11 @@ var Amslib_Simple_Fader_Slideshow = my.Amslib_Simple_Fader_Slideshow = my.Class(
 	
 	animate: function()
 	{
-		var active		=	$("img.active",this.parent);
-		var inactive	=	active.next("img");
+		var selector	=	Amslib_Simple_Fader_Slideshow.config.cselector;
+		var active		=	$(selector+".active",this.parent);
+		var inactive	=	active.next(selector);
 		
-		if(!inactive.length) inactive = $("img:first",this.parent);
+		if(!inactive.length) inactive = $(selector+":first",this.parent);
 		
 		//	NOTE: disabled until Amslib_Event is rewritten
 		//this.callObserver("fade-start",inactive);
