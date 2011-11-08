e818bec353c859dbe6d8dc649f9c5255b786c555
Christopher Thomas <chris.thomas@antimatter-studios.com>
2011-11-08 22:49:38 +0100

    -	rearrange the CKEditor loader because it wasn't working and sometimes the resources loaded in the wrong order, causing things to start failing.Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- util/Amslib_CKEditor.js
+++ util/Amslib_CKEditor.js
@@ -8,22 +8,25 @@ var path = Amslib.getPath("/util/Amslib_CKEditor.js");
 
 if(path){
 	Amslib.loader.ckeditor			=	require(path+"/util/ckeditor/ckeditor.js");
-	Amslib.loader.ckeditor_jquery	=	require(path+"/util/ckeditor/adapters/jquery.js");
 	
 	scope.ready(function(){
-		CKEDITOR.config.toolbar_CKeditor_Config_1 = Amslib_CKEditor[0];
-		CKEDITOR.config.toolbar_CKeditor_Config_2 = Amslib_CKEditor[1];
-		CKEDITOR.config.toolbar_CKeditor_Config_3 = Amslib_CKEditor[2];
+		Amslib.loader.ckeditor_jquery	=	require(path+"/util/ckeditor/adapters/jquery.js");
 		
-		$(".ckeditor_noconfig").ckeditor();
-		//	These are still here for compatibility with old code until they can be updated
-		$(".ckeditor_simple_1").ckeditor(function(){},{toolbar:"CKeditor_Config_1"});
-		$(".ckeditor_simple_2").ckeditor(function(){},{toolbar:"CKeditor_Config_2"});
-		//	These are the new way to apply a configuration
-		$(".ckeditor_config_1").ckeditor(function(){},{toolbar:"CKeditor_Config_1"});
-		$(".ckeditor_config_2").ckeditor(function(){},{toolbar:"CKeditor_Config_2"});
-		$(".ckeditor_config_3").ckeditor(function(){},{toolbar:"CKeditor_Config_3"});
-	},Amslib.loader.ckeditor,Amslib.loader.ckeditor_jquery);
+		scope(function(){
+			CKEDITOR.config.toolbar_CKeditor_Config_1 = Amslib_CKEditor[0];
+			CKEDITOR.config.toolbar_CKeditor_Config_2 = Amslib_CKEditor[1];
+			CKEDITOR.config.toolbar_CKeditor_Config_3 = Amslib_CKEditor[2];
+			
+			$(".ckeditor_noconfig").ckeditor();
+			//	These are still here for compatibility with old code until they can be updated
+			$(".ckeditor_simple_1").ckeditor(function(){},{toolbar:"CKeditor_Config_1"});
+			$(".ckeditor_simple_2").ckeditor(function(){},{toolbar:"CKeditor_Config_2"});
+			//	These are the new way to apply a configuration
+			$(".ckeditor_config_1").ckeditor(function(){},{toolbar:"CKeditor_Config_1"});
+			$(".ckeditor_config_2").ckeditor(function(){},{toolbar:"CKeditor_Config_2"});
+			$(".ckeditor_config_3").ckeditor(function(){},{toolbar:"CKeditor_Config_3"});
+		},Amslib.loader.ckeditor_jquery);
+	},Amslib.loader.ckeditor);
 }
 
 Amslib_CKEditor = [
