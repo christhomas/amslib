07d0aee1cea993848a87fa5e30f6e818a074c190
Christopher Thomas <chris.thomas@antimatter-studios.com>
2012-04-01 11:51:51 +0200

    install the full phpQuery and QueryPath libraries to see which one I prefer and update to 12.3 (although I think that bpremium has 12.6 :p )Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- Amslib.php
+++ Amslib.php
@@ -166,7 +166,7 @@ class Amslib
 
 		return ($preformat) ? "<pre $hiddenOutput>$dump</pre>" : $dump;
 	}
-	
+
 	//	Website: http://php.net
 	//	User: Levofski
 	//	Link: http://www.php.net/manual/en/function.print-r.php#97901
@@ -175,13 +175,13 @@ class Amslib
 	{
 	    // capture the output of print_r
 	    $out = print_r($mixed, true);
-	   
+
 	    // Replace the root item with a struct
 	    // MATCH : '<start>element<newline> ('
 	    $root_pattern = '/[ \t]*([a-z0-9 \t_]+)\n[ \t]*\(/i';
 	    $root_replace_pattern = '<struct name="root" type="\\1">';
 	    $out = preg_replace($root_pattern, $root_replace_pattern, $out, 1);
-	
+
 	    // Replace array and object items structs
 	    // MATCH : '[element] => <newline> ('
 	    $struct_pattern = '/[ \t]*\[([^\]]+)\][ \t]*\=\>[ \t]*([a-z0-9 \t_]+)\n[ \t]*\(/miU';
@@ -189,16 +189,16 @@ class Amslib
 	    $out = preg_replace($struct_pattern, $struct_replace_pattern, $out);
 	    // replace ')' on its own on a new line (surrounded by whitespace is ok) with '</var>
 	    $out = preg_replace('/^\s*\)\s*$/m', '</struct>', $out);
-	   
+
 	    // Replace simple key=>values with vars
 	    // MATCH : '[element] => value<newline>'
 	    $var_pattern = '/[ \t]*\[([^\]]+)\][ \t]*\=\>[ \t]*([a-z0-9 \t_\S]+)/i';
 	    $var_replace_pattern = '<var name="\\1">\\2</var>';
 	    $out = preg_replace($var_pattern, $var_replace_pattern, $out);
-	   
+
 	    $out =  trim($out);
 	    $out='<?xml version="1.0"?><data>'.$out.'</data>';
-	
+
 	    return $out;
 	}
 
@@ -282,12 +282,13 @@ class Amslib
 			if(strpos($c,"Amslib_Mixin")		!== false)	$c	=	"mvc/$c";
 			if(strpos($c,"Amslib_File")			!== false)	$c	=	"file/$c";
 			if(strpos($c,"CakePHP")				!== false)	$c	=	"util/$c";
+			if(strpos($c,"phpQuery")			!== false)	$c	=	"util/$c/$c";
+			if(strpos($c,"QueryPath")			!== false)	$c	=	"util/$c/$c";
 
 			//	DEPRECATED: unless I can find a way to fix the utf-8 broken characters like ü
 			if(strpos($c,"HtmlCutString")		!== false)	$c	=	"util/html_cut_string";
 
 			if($c == "FirePHP")		$c	=	"util/FirePHPCore/$c.class";
-			if($c == "phpQuery")	$c =	"util/phpquery";
 
 			$f = str_replace("//","/","$c.php");
 
