53ce4c75843c68582c32dcffec38b3701adb7c72
Christopher Thomas <chris.thomas@antimatter-studios.com>
2013-02-19 18:01:15 +0100

    make a fix for the saveUploadedFile method where it would "break" the file extension by replacing .pdf with _pdf making it unable to open on some computers, the fix breaks up the string, grabs the last one as the extension, implodes the rest and attached the other after it's all imploded and slugified
    Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- file/Amslib_File.php
+++ file/Amslib_File.php
@@ -187,7 +187,9 @@ class Amslib_File
 			$error = true;
 		}
 
-		$dst_filename	= Amslib::slugify($dst_filename,"","_");
+		$parts			= explode(".",$dst_filename);
+		$extension		= array_pop($parts);
+		$dst_filename	= Amslib::slugify(implode(".",$parts),"","_").$extension;
 		$destination	= self::reduceSlashes("$directory/$dst_filename");
 
 		//	Try to move the file into the correct destination
