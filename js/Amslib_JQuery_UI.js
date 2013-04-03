0ec99c2327215efc71164484eb55d85b1e32499f
Christopher Thomas <chris.thomas@antimatter-studios.com>
2013-04-03 10:20:43 +0200

    clean and fix up the jqueryui styles to be more uniform, etc.
    Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- js/Amslib_JQuery_UI.js
+++ js/Amslib_JQuery_UI.js
@@ -2,7 +2,7 @@
 This script will automatically include all the other resources without any fuss
 */
 
-if(Amslib == undefined || window.exports == undefined) throw("Amslib_JQuery_UI.js: requires amslib/my.common to be loaded first");
+if(Amslib == undefined || window.exports == undefined) throw("Amslib_JQuery_UI.js: requires Amslib.js+my.class.min.js to be loaded first");
 
 var amslib = Amslib.locate();
 
@@ -10,14 +10,9 @@ if(amslib){
 	//	FIXME: It looks from the method functionality that these two parameters should be swapped over
 	//	NOTE: also logically it makes more sense that they are reversed also.
 	var theme = Amslib.getQuery("theme",Amslib.getJSPath("Amslib_JQuery_UI.js"));
-
-	var themeList = {
-		"smoothness":	amslib+"/css/jqueryui/smoothness/jquery-ui-1.8.14.custom.css",
-		"aristo":		amslib+"/css/jqueryui/Aristo/Aristo.css"
-	};
 	
-	if(!themeList[theme]) theme = "smoothness";
-
-	Amslib.loadCSS(themeList[theme]);
+	//	FIXME: what to do if the selected theme is not available? nothing?
+	
+	Amslib.loadCSS(amslib+"/util/jqueryui/"+theme+"/styles.css");
 	Amslib.loadJS("jquery.ui",amslib+"/util/jquery-ui-1.8.14.custom.min.js");
 };
