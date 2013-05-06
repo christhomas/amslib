0d87a51019801f172f939cb2dbc09f1b0912e055
Christopher Thomas <chris.thomas@antimatter-studios.com>
2013-05-06 19:22:33 +0200

    update with the new translator database definition
    Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- translator/amstudios_translator.sql
+++ translator/amstudios_translator.sql
@@ -1,8 +1,9 @@
-create table amstudios_translator (
-	id		int not null auto_increment,
-	lang	varchar(5),
-	k		varchar(255),
-	v		text,
+create table amstudios_translation (
+	id			int not null auto_increment,
+	lang		varchar(5),
+	object_id	int not null,
+	name		varchar(255),
+	value		text,
 	
 	primary key (id)
-) type=InnoDB, CHARACTER SET UTF8;
+) Engine=InnoDB, CHARACTER SET UTF8;
