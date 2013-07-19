09d093b03c23d5e31d2a459911ecc16a266611d8
Christopher Thomas <chris.thomas@antimatter-studios.com>
2013-07-19 12:27:36 +0200

    remove header() call and replace it with call to Amslib_Website::redirect
    Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- Amslib_Paypal.php
+++ Amslib_Paypal.php
@@ -19,7 +19,7 @@
 *    {Christopher Thomas} - Creator - chris.thomas@antimatter-studios.com
 *
 *******************************************************************************/
- 
+
 /**
  * 	class:	Amslib_Paypal
  *
@@ -31,7 +31,7 @@
  *
  * 	todo: write documentation
  * 	UNKNOWN PARAMETERS FROM NVP EXAMPLE
- * 
+ *
  * 	$nvpstr =
  * 		MAXAMT=$maxamt&
  * 		AMT=(string)$am"&
@@ -45,49 +45,49 @@ class Amslib_Paypal
 	 * $url:	URL to call when executing operation
 	 */
 	protected $url;
-	
+
 	/**
 	 * $nvpURL: the url for paypal login
 	 */
 	protected $nvpURL;
-	
+
 	/**
 	 * $paypalURL: the normal url to post other operation to
 	 */
 	protected $paypalURL;
-	
+
 	/**
 	 * $authenticationMode: 3token(Username, Password and Signature)
 	 */
 	protected $authenticationMode = "3TOKEN";
-	
+
 	/**
 	 * params: the parameters that the remote call will need to process the request
-	 * 
+	 *
 	 * note:
 	 * 	-	we keep this separate from products because we will need to update the products array dynamically
 	 */
 	protected $params;
-	
+
 	/**
 	 * $products: The array of products that are involved in this transaction
 	 */
 	protected $products;
-	
+
 	/**
 	 * $queryString: The final string to send to the url
 	 */
 	protected $queryString;
-	
+
 	protected $response;
-	
+
 	protected $debug;
-	
+
 	/**
 	 * method: calculateProductTotal
-	 * 
+	 *
 	 * Calculate the total price for the purchase, from the amounts specified in the products array
-	 * 
+	 *
 	 * notes:
 	 * 	-	we need to add the tax amount also?
 	 * 	-	we need to add the shipping amount also?
@@ -96,17 +96,17 @@ class Amslib_Paypal
 	protected function calculateProductTotal()
 	{
 		$amount = (float)0;
-		
+
 		foreach($this->products as $num=>$p)
 		{
 			$amount += $p["L_AMT{$num}"]*$p["L_QTY{$num}"];
 		}
-		
+
 		//	Add tax amount?
 		//	Add shipping costs?
 		$this->setAmountTotal($amount);
 	}
-	
+
 	/**
 	 * 	method:	checkRequiredFields
 	 *
@@ -119,18 +119,18 @@ class Amslib_Paypal
 				if($this->get("amt") == false){
 					$this->calculateProductTotal();
 				}
-				
+
 				if($this->get("paymentaction") == false){
-					$this->setPaymentAction("sale");	
+					$this->setPaymentAction("sale");
 				}
-				
+
 				if($this->get("noshipping") == false){
 					$this->set("noshipping",1);
 				}
 			}break;
 		}
 	}
-	
+
 	/**
 	 * 	method:	collapseParameters
 	 *
@@ -140,13 +140,13 @@ class Amslib_Paypal
 	{
 		//	Collapse all the parameters set manually
 		$this->queryString = http_build_query($this->params);
-		
+
 		//	Collapse the products into the same query string
 		foreach($this->products as $p){
 			$this->queryString .= "&".http_build_query($p);
 		}
-	}	
-	
+	}
+
 	/**
 	 * 	method:	decodeResponse
 	 *
@@ -155,22 +155,22 @@ class Amslib_Paypal
 	protected function decodeResponse($response)
 	{
 		if($response == false) return false;
-		
+
 		if(is_string($response)){
 			if($this->debug) print("DEBUGGING: ".var_dump($response));
-			
+
 			$parameters = explode("&",$response);
-			
+
 			$this->response = array();
 			foreach($parameters as $p){
 				list($key,$value) = explode("=",$p);
-				
+
 				$this->response[urldecode($key)] = urldecode($value);
 			}
 			return ($this->getResponse("ACK") == "Success") ? true : false;
 		}
 	}
-	
+
 	/**
 	 * 	method:	__construct
 	 *
@@ -179,10 +179,10 @@ class Amslib_Paypal
 	public function __construct()
 	{
 		$this->initialise();
-		
+
 		$this->setDebug(false);
 	}
-	
+
 	/**
 	 * 	method:	setDebug
 	 *
@@ -192,7 +192,7 @@ class Amslib_Paypal
 	{
 		$this->debug = $state;
 	}
-	
+
 	/**
 	 * 	method:	initialise
 	 *
@@ -203,7 +203,7 @@ class Amslib_Paypal
 		$this->params		=	array();
 		$this->products		=	array();
 	}
-	
+
 	/**
 	 * 	method:	setAPIDetails
 	 *
@@ -215,7 +215,7 @@ class Amslib_Paypal
 		$this->set("pwd",		urlencode($password));
 		$this->set("signature",	urlencode($signature));
 	}
-	
+
 	/**
 	 * 	method:	enableSandbox
 	 *
@@ -226,7 +226,7 @@ class Amslib_Paypal
 		$this->nvpURL		=	"https://api-3t.sandbox.paypal.com/nvp";
 		$this->paypalURL	=	"https://www.sandbox.paypal.com/webscr";
 	}
-	
+
 	/**
 	 * 	method:	enableLive
 	 *
@@ -237,7 +237,7 @@ class Amslib_Paypal
 		$this->nvpURL		=	"https://api-3t.paypal.com/nvp";
 		$this->paypalURL	=	"https://www.paypal.com/webscr";
 	}
-	
+
 	/**
 	 * 	method:	setExpressCheckout
 	 *
@@ -256,7 +256,7 @@ class Amslib_Paypal
 			$this->set("token",$this->getResponse("TOKEN"));
 		}
 	}
-	
+
 	/**
 	 * 	method:	getExpressCheckoutDetails
 	 *
@@ -267,9 +267,9 @@ class Amslib_Paypal
 		$this->url		=	$this->nvpURL;
 		$this->command	=	"GetExpressCheckoutDetails";
 		$this->set("method",urlencode($this->command));
-		$this->setToken($token);		
+		$this->setToken($token);
 	}
-	
+
 	/**
 	 * 	method:	doExpressCheckout
 	 *
@@ -287,7 +287,7 @@ class Amslib_Paypal
 		$this->setAmountTotal($this->getResponse("AMT"));
 		$this->setPaymentAction("sale");
 	}
-	
+
 	/**
 	 * 	method:	set
 	 *
@@ -297,7 +297,7 @@ class Amslib_Paypal
 	{
 		$this->params[$name] = $value;
 	}
-	
+
 	/**
 	 * 	method:	get
 	 *
@@ -307,7 +307,7 @@ class Amslib_Paypal
 	{
 		return (isset($this->params[$name])) ? $this->params[$name] : false;
 	}
-	
+
 	/**
 	 * 	method:	setIPAddress
 	 *
@@ -317,7 +317,7 @@ class Amslib_Paypal
 	{
 		$this->set("ipaddress",urlencode($address));
 	}
-	
+
 	/**
 	 * 	method:	setToken
 	 *
@@ -329,10 +329,10 @@ class Amslib_Paypal
 		if($token == false) return false;
 
 		$this->set("token",urlencode($token));
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * 	method:	setPayerId
 	 *
@@ -342,12 +342,12 @@ class Amslib_Paypal
 	{
 		if($payerId == false) $payerId = $this->getResponse("PAYERID");
 		if($payerId == false) return false;
-		
+
 		$this->set("payerid",urlencode($payerId));
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * 	method:	addProduct
 	 *
@@ -356,10 +356,10 @@ class Amslib_Paypal
 	public function addProduct($name,$description,$productId,$quantity,$price)
 	{
 		$num = count($this->products);
-		
+
 		$quantity	=	(float)$quantity;
 		$price		=	(float)$price;
-		
+
 		$productData = array(
 			"L_NAME{$num}"		=>	$name,
 			"L_DESC{$num}"		=>	$description,
@@ -367,12 +367,12 @@ class Amslib_Paypal
 			"L_QTY{$num}"		=>	$quantity,
 			"L_AMT{$num}"		=>	$price,
 		);
-		
+
 		$this->products[$num] = $productData;
-		
+
 		return $num;
 	}
-	
+
 	/**
 	 * 	method:	setShippingDetails
 	 *
@@ -386,13 +386,13 @@ class Amslib_Paypal
 		$this->set("shiptocity",		$city);
 		$this->set("shiptocountrycode",	$countryCode);
 		$this->set("shiptozip",			$postcode);
-		
+
 		//	This can be optional in many cases, so test for it being empty
 		if(strlen($state)){
 			$this->set("shiptostate",	$state);
 		}
 	}
-	
+
 	/**
 	 * 	method:	setShippingAmount
 	 *
@@ -403,7 +403,7 @@ class Amslib_Paypal
 		$this->set("shippingamt",$shippingAmount);
 		$this->set("shipdiscamt",$shippingDiscount);
 	}
-	
+
 	/**
 	 * 	method:	setPhoneNumber
 	 *
@@ -413,7 +413,7 @@ class Amslib_Paypal
 	{
 		$this->set("shiptophonenum",$number);
 	}
-	
+
 	/**
 	 * 	method:	setEmail
 	 *
@@ -423,19 +423,19 @@ class Amslib_Paypal
 	{
 		$this->set("email",$email);
 	}
-	
+
 	/**
 	 * method: setProductWeight
-	 * 
+	 *
 	 * set the products weight so you can calculate the shipping
-	 * 
+	 *
 	 * parameters:
 	 * 	$pid	-	The item number in the product array (was returned by the method addProduct)
 	 * 	$unit	-	The unit of weight you are using
 	 * 	$weight	-	The weight of the product, using the units provided
-	 * 
+	 *
 	 * notes:
-	 * 	-	the item about calculating the shipping is that you provide us with enough 
+	 * 	-	the item about calculating the shipping is that you provide us with enough
 	 * 		information so that this is actually possible
 	 */
 	public function setProductWeight($pid,$unit,$weight)
@@ -445,18 +445,18 @@ class Amslib_Paypal
 			$this->products[$pid]["L_ITEMWEIGHTVALUE{$pid}"]	=	$weight;
 		}
 	}
-	
+
 	/**
 	 * method: setProductShipping
-	 * 
+	 *
 	 * Set a shipping detail against a particular product
-	 * 
+	 *
 	 * parameters:
 	 * 	$pid	-	The id of the product (as returned by addProduct)
 	 * 	$label	-	The label of the shipping type to use
 	 * 	$name	-	The name of the shipping type
 	 * 	$amount	-	The amount the shipping will cost
-	 * 
+	 *
 	 * notes:
 	 * 	-	I am not sure how to validate these parameters to make sure they are valid, or whether
 	 * 		there are invalid values to pass, so be careful how to use this method
@@ -470,7 +470,7 @@ class Amslib_Paypal
 			$this->products[$pid]["L_SHIPPINGOPTIONAMOUNT{$pid}"]		=	$amount;
 		}
 	}
-	
+
 	/**
 	 * 	method:	setPaymentAction
 	 *
@@ -479,12 +479,12 @@ class Amslib_Paypal
 	public function setPaymentAction($action)
 	{
 		$allowed = array("sale","offer","authorization");
-		
+
 		if(in_array($action,$allowed)){
 			$this->set("paymentaction",$action);
 		}
 	}
-	
+
 	/**
 	 * 	method:	setLocaleCode
 	 *
@@ -493,12 +493,12 @@ class Amslib_Paypal
 	public function setLocaleCode($code)
 	{
 		$allowed = array("AU","DE","FR","GB","IT","ES","JP","US");
-		
+
 		if(in_array($code,$allowed)){
 			$this->set("locale",$code);
 		}
 	}
-	
+
 	/**
 	 * 	method:	setCurrencyCode
 	 *
@@ -514,12 +514,12 @@ class Amslib_Paypal
 			"NOK","NZD","PLN",
 			"SEK","SGD","USD"
 		);
-		
+
 		if(in_array($currencyCode,$allowed)){
 			$this->set("currencycode",$currencyCode);
 		}
 	}
-	
+
 	/**
 	 * 	method:	setAPIVersion
 	 *
@@ -529,7 +529,7 @@ class Amslib_Paypal
 	{
 		$this->set("version",urlencode($version));
 	}
-	
+
 	/**
 	 * 	method:	setAmountTotal
 	 *
@@ -538,10 +538,10 @@ class Amslib_Paypal
 	public function setAmountTotal($amount)
 	{
 		if(!$amount || !is_numeric($amount)) return false;
-		
+
 		$this->set("amt",$amount);
 	}
-	
+
 	/**
 	 * 	method:	setTaxAmount
 	 *
@@ -550,10 +550,10 @@ class Amslib_Paypal
 	public function setTaxAmount($amount)
 	{
 		if(!$amount || !is_numeric($amount)) return false;
-		
+
 		$this->set("taxamt",$amount);
 	}
-	
+
 	/**
 	 * 	method:	setReturnURL
 	 *
@@ -563,7 +563,7 @@ class Amslib_Paypal
 	{
 		$this->set("returnurl",$url);
 	}
-	
+
 	/**
 	 * 	method:	setCancelURL
 	 *
@@ -573,7 +573,7 @@ class Amslib_Paypal
 	{
 		$this->set("cancelurl",$url);
 	}
-	
+
 	/**
 	 * 	method:	setInsurance
 	 *
@@ -582,11 +582,11 @@ class Amslib_Paypal
 	public function setInsurance($amount)
 	{
 		if(!$amount || !is_numeric($amount)) return false;
-		
+
 		$this->set("insuranceoptionoffered",true);
 		$this->set("insuranceamt",$amount);
 	}
-	
+
 	/**
 	 * 	method:	execute
 	 *
@@ -604,17 +604,17 @@ class Amslib_Paypal
 
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 		curl_setopt($ch, CURLOPT_POST, 1);
-		
+
 		$this->checkRequiredFields();
 		$this->collapseParameters();
-		
+
 		if($this->debug) print("$this->url?$this->queryString<br/>");
-		
+
 		//setting the nvpreq as POST FIELD to curl
 		curl_setopt($ch,CURLOPT_POSTFIELDS,$this->queryString);
-		
+
 		$r = curl_exec($ch);
-		
+
 		if (curl_errno($ch)) {
 			//	moving to display page to display curl errors
 			print("FAILURE INFO: ".curl_errno($ch)."<br/>");
@@ -625,7 +625,7 @@ class Amslib_Paypal
 		//getting response from server
 		return $this->decodeResponse($r);
 	}
-	
+
 	/**
 	 * 	method:	getResponse
 	 *
@@ -636,10 +636,10 @@ class Amslib_Paypal
 		if($field){
 			return (isset($this->response[$field])) ? $this->response[$field] : false;
 		}
-		
+
 		return $this->response;
 	}
-	
+
 	/**
 	 * 	method:	getError
 	 *
@@ -649,12 +649,12 @@ class Amslib_Paypal
 	{
 		$r = $this->getResponse();
 		if(is_string($r)){
-			print("<pre>response[string]: ".htmlentities($r)."</pre>");	
+			print("<pre>response[string]: ".htmlentities($r)."</pre>");
 		}else{
 			print("<pre>response[array]: ".print_r($r,true)."</pre>");
 		}
 	}
-	
+
 	/**
 	 * 	method:	redirect
 	 *
@@ -670,8 +670,7 @@ class Amslib_Paypal
 				$this->collapseParameters();
 
 				$this->url = "$this->url?$this->queryString";
-				header("Location: $this->url");
-				die("WAITING TO REDIRECT: '$this->url'");
+				Amslib_Website::redirect($this->url);
 			}
 		}
 	}
