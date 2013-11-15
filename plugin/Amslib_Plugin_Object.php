e14ca4ebce51c7aebfa82c95dad7d8bb25e21f54
Christopher Thomas <chris.thomas@antimatter-studios.com>
2013-11-15 17:33:02 +0100

    remove some application code I left in the Amslib_Plugin_Object object by accident
    Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- plugin/Amslib_Plugin_Object.php
+++ plugin/Amslib_Plugin_Object.php
@@ -19,11 +19,8 @@ class Amslib_Plugin_Object
 		return $this->api ? true : false;
 	}
 
-	//	NOTE: in an ideal world, this method would be handled by the inheritance
 	public function initialiseObject($api)
 	{
 		$this->api = $api;
-
-		$this->model = $this->api->getObject("Mo_Catalog");
 	}
 }
