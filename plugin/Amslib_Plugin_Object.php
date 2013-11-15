104d137674b09c4c036b9e34dc10dc5fc58924d1
Christopher Thomas <chris.thomas@antimatter-studios.com>
2013-11-15 12:51:29 +0100

    add this object because I wanted to reduce the amount of methods I manually copy into the new controller objects before I go through a refactoring phase
    Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

---
+++ plugin/Amslib_Plugin_Object.php
@@ -0,0 +1,29 @@
+<?php
+class Amslib_Plugin_Object
+{
+	protected $api;
+
+	public function __construct()
+	{
+		$this->api = false;
+	}
+
+	/**
+	 * 	method:	isInitialised
+	 *
+	 * 	todo: write documentation
+	 */
+	public function isInitialised()
+	{
+		//	FIXME: this is overly simplistic, but quite realiable.
+		return $this->api ? true : false;
+	}
+
+	//	NOTE: in an ideal world, this method would be handled by the inheritance
+	public function initialiseObject($api)
+	{
+		$this->api = $api;
+
+		$this->model = $this->api->getObject("Mo_Catalog");
+	}
+}
