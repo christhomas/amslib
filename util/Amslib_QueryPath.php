491798ceab93337625343e966ed9400065582ee2
Christopher Thomas <chris.thomas@antimatter-studios.com>
2013-12-12 19:05:31 +0100

    make modifications to the query path object to hide errors that happen, we're going to output text to the error log, but not break the page layout and perhaps this information can go into some kind of visible warning system without screwing everything up, but at least we get the best of both worlds here
    Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- util/Amslib_QueryPath.php
+++ util/Amslib_QueryPath.php
@@ -10,15 +10,27 @@ class Amslib_QueryPath
 		//	it's such a hack :P
 		require_once(dirname(__FILE__)."/QueryPath/QueryPath.php");
 
-		self::$qp = qp($document,$selector,$options);
+		try{
+			ob_start();
+			self::$qp = qp($document,$selector,$options);
+			$warnings = ob_get_clean();
 
-		return self::$qp;
+			if(strlen($warnings)){
+				Amslib::errorLog("FAILED TO OBTAIN CLEAN OUTPUT WHEN PROCESSING DOCUMENT: error = ",$warnings);
+			}else{
+				return self::$qp;
+			}
+		}catch(Exception $e){
+			//	I dunno what to do here
+		}
+
+		return qp();
 	}
-	
+
 	static public function htmlqp($document=NULL,$selector=NULL,$options=array())
 	{
 		// I copied and modified the default options from the htmlqp method to provide a custom version for Amslib
-		
+
 		if(is_string($document)) $document = htmlspecialchars($document,ENT_COMPAT,"UTF-8");
 
 		$options += array(
@@ -27,8 +39,20 @@ class Amslib_QueryPath
 			'convert_from_encoding'		=>	'auto',
 			'use_parser'				=>	'html'
 		);
-		
-		return self::qp($document, $selector, $options);
+
+		try{
+			ob_start();
+				$html = self::qp($document, $selector, $options);
+			$warnings = ob_get_clean();
+
+			if(strlen($warnings)){
+				Amslib::errorLog("FAILED TO OBTAIN CLEAN OUTPUT WHEN PROCESSING HTML: error = ",$warnings);
+			}
+		}catch(Exception $e){
+			//	I dunno what to do here
+		}
+
+		return $html;
 	}
 
 	static public function get()
