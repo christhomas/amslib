19d930045701e28b475209da7801cc66fa82a6b8
Christopher Thomas <chris.thomas@antimatter-studios.com>
2014-02-24 16:25:22 +0100

    update the $_w parameter for $wt, because it's easier, although it means $wt isn't an available parameter without overwriting the translator with another, so might cause problems.
    perhaps I can add a warning when you do this, then it'll be something I
    can warn against
    
    Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- templates/xml_translation/views/Vi_XML_Translation.php
+++ templates/xml_translation/views/Vi_XML_Translation.php
@@ -14,17 +14,17 @@
 
 <h1>English or Spanish</h1>
 <ul>
-	<li>this_is_some: <?=$_w->t("this_is_some")?></li>
-	<li>you_put_the: <?=$_w->t("you_put_the")?></li>
-	<li>and_then_put: <?=$_w->t("and_then_put")?></li>
-	<li>of_each_translation: <?=$_w->t("of_each_translation")?></li>
-	<li>but_it_is_important: <?=$_w->t("but_it_is_important")?></li>
-	<li>in_how_to_arrange: <?=$_w->t("in_how_to_arrange")?></li>
-	<li>so_that_your_code: <?=$_w->t("so_that_your_code")?></li>
-	<li>so_there_are_some_rules: <?=$_w->t("so_there_are_some_rules")?></li>
-	<li>in_how_to_arrange_your_name_keys: <?=$_w->t("in_how_to_arrange_your_name_keys")?></li>
-	<li>doesnt_break_down: <?=$_w->t("doesnt_break_down")?></li>
-	<li>for_the_name_try: <?=$_w->t("for_the_name_try")?></li>
-	<li>dont_put_spaces: <?=$_w->t("dont_put_spaces")?></li>
-	<li>and_this_will_keep: <?=$_w->t("and_this_will_keep")?></li>
+	<li>this_is_some: <?=$wt->t("this_is_some")?></li>
+	<li>you_put_the: <?=$wt->t("you_put_the")?></li>
+	<li>and_then_put: <?=$wt->t("and_then_put")?></li>
+	<li>of_each_translation: <?=$wt->t("of_each_translation")?></li>
+	<li>but_it_is_important: <?=$wt->t("but_it_is_important")?></li>
+	<li>in_how_to_arrange: <?=$wt->t("in_how_to_arrange")?></li>
+	<li>so_that_your_code: <?=$wt->t("so_that_your_code")?></li>
+	<li>so_there_are_some_rules: <?=$wt->t("so_there_are_some_rules")?></li>
+	<li>in_how_to_arrange_your_name_keys: <?=$wt->t("in_how_to_arrange_your_name_keys")?></li>
+	<li>doesnt_break_down: <?=$wt->t("doesnt_break_down")?></li>
+	<li>for_the_name_try: <?=$wt->t("for_the_name_try")?></li>
+	<li>dont_put_spaces: <?=$wt->t("dont_put_spaces")?></li>
+	<li>and_this_will_keep: <?=$wt->t("and_this_will_keep")?></li>
 </ul>
