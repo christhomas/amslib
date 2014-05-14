baa6ae0d68abd98c8a7b7a3a00e95a7feed62136
Christopher Thomas <chris.thomas@antimatter-studios.com>
2014-05-14 13:36:22 +0200

    add a formatJSON method to the Amslib object to format json strings into something a human can read
    Signed-off-by: Christopher Thomas <chris.thomas@antimatter-studios.com>

--- Amslib.php
+++ Amslib.php
@@ -155,6 +155,93 @@ class Amslib
 	}
 
 	/**
+	 *	method: formatJSON
+	 *
+	 *	A method to pretty format json output so you can output it somewhere
+	 *
+	 *	parameters:
+	 *		$json	-	The json that you'd like to format
+	 *
+	 *	notes:
+	 *		-	You can pass an array and it'll convert it to json using json_encode($json)
+	 *		-	I got this code from here: http://stackoverflow.com/a/9776726/279147
+	 *		-	Thanks a lot!!
+	 *
+	 *	returns:
+	 *		A string containing the json output, or "FALSE: json invalid" when it fails
+	 */
+	public function formatJSON($json)
+	{
+		$error = "FALSE: json invalid";
+
+		if(is_array($json)){
+			try{
+				$json = json_encode($json);
+			}catch(Exception $e){
+				return $error;
+			}
+		}
+
+		if(!is_string($json)){
+			return $error;
+		}
+
+		$result = '';
+		$level = 0;
+		$in_quotes = false;
+		$in_escape = false;
+		$ends_line_level = NULL;
+		$json_length = strlen( $json );
+
+		for( $i = 0; $i < $json_length; $i++ ) {
+			$char = $json[$i];
+			$new_line_level = NULL;
+			$post = "";
+			if( $ends_line_level !== NULL ) {
+				$new_line_level = $ends_line_level;
+				$ends_line_level = NULL;
+			}
+			if ( $in_escape ) {
+				$in_escape = false;
+			} else if( $char === '"' ) {
+				$in_quotes = !$in_quotes;
+			} else if( ! $in_quotes ) {
+				switch( $char ) {
+					case '}': case ']':
+						$level--;
+						$ends_line_level = NULL;
+						$new_line_level = $level;
+						break;
+
+					case '{': case '[':
+						$level++;
+					case ',':
+						$ends_line_level = $level;
+						break;
+
+					case ':':
+						$post = " ";
+						break;
+
+					case " ": case "\t": case "\n": case "\r":
+						$char = "";
+						$ends_line_level = $new_line_level;
+						$new_line_level = NULL;
+						break;
+				}
+			} else if ( $char === '\\' ) {
+				$in_escape = true;
+			}
+			if( $new_line_level !== NULL ) {
+				$result .= "\n".str_repeat( "\t", $new_line_level );
+			}
+			$result .= $char.$post;
+		}
+
+		return $result;
+	}
+
+	/**
 	 * 	method:	lchop
 	 *
 	 * 	Chop a string to remove everything to the left of the
