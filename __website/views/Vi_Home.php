1cec420019646e20c49a2297fb7128b85a508bc3
Christopher Thomas <chris.thomas@antimatter-studios.com>
2014-10-05 19:20:09 +0200

    -	add all the favicons for the various devices -	make the templates work, but without an api object for now -	add a little bit of content

---
+++ __website/views/Vi_Home.php
@@ -0,0 +1,7 @@
+<section>
+	<img class="pull-left" src="<?=$api->getImage("/__website/resources/logo.png")?>" alt="Antimatter Studios" />
+
+	<h1>We are running the amslib framework website</h1>
+
+	<h2><i>Coming soon, in time we'll fill this with content</i></h2>
+</section>
--- __website/views/Vi_Skeleton.php
+++ __website/views/Vi_Skeleton.php
@@ -8,7 +8,15 @@
 
 	<title>AMSLIB: <?=$api->getValue("title")?></title>
 
-	<link rel="shortcut icon" href="<?=$api->getImage("/__website/resources/favicon.ico")?>" />
+	<link rel="shortcut icon" href="<?=$api->getImage("/__website/favicon/favicon.ico")?>" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="<?=$api->getImage("/__website/favicon/apple-touch-icon.png")?>" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?=$api->getImage("/__website/favicon/apple-touch-icon-57x57.png")?>" />
+	<link rel="apple-touch-icon" sizes="72x72" href="<?=$api->getImage("/__website/favicon/apple-touch-icon-72x72.png")?>" />
+	<link rel="apple-touch-icon" sizes="76x76" href="<?=$api->getImage("/__website/favicon/apple-touch-icon-76x76.png")?>" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?=$api->getImage("/__website/favicon/apple-touch-icon-114x114.png")?>" />
+	<link rel="apple-touch-icon" sizes="120x120" href="<?=$api->getImage("/__website/favicon/apple-touch-icon-120x120.png")?>" />
+	<link rel="apple-touch-icon" sizes="144x144" href="<?=$api->getImage("/__website/favicon/apple-touch-icon-144x144.png")?>" />
+	<link rel="apple-touch-icon" sizes="152x152" href="<?=$api->getImage("/__website/favicon/apple-touch-icon-152x152.png")?>" />
 
 	<?=Amslib_Resource::getStylesheet()?>
 	<?=Amslib_Resource::getJavascript()?>
@@ -30,18 +38,11 @@
         <!-- /.container -->
     </nav>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <!--[if lt IE 7]>
-				<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-				<![endif]-->
+    <main class="container-fluid">
+		<!--[if lt IE 7]>
+		<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+		<![endif]-->
 
-				<!-- Add your site or application content here -->
-				<h1>We are running the amslib framework website</h1>
-            </div>
-        </div>
-
-    </div>
-    <!-- /.container -->
+		<?=$api->renderView($api->getRouteParam("template"))?>
+    </main>
 </body>
