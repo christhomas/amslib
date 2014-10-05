bd5c2856d9f773d715fc4403d63a199f760834d6
Christopher Thomas <chris.thomas@antimatter-studios.com>
2014-10-05 23:33:32 +0200

    small fixups for the navigation bar

--- __website/views/Vi_Skeleton.php
+++ __website/views/Vi_Skeleton.php
@@ -26,13 +26,12 @@
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">Antimatter Studios / AMSLIB Framework Website</a>
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="glyphicon glyphicon-align-justify"></span>
+				</button>
+
+                <a class="navbar-brand" href="index.php">Amslib Framework</a>
             </div>
         </div>
         <!-- /.container -->
