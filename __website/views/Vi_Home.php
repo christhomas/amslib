915791117c3eee9f15ff93fc0c589d26fd6fa1dd
Christopher Thomas <chris.thomas@antimatter-studios.com>
2014-12-16 00:47:39 +0100

    fix up the minisite with new rules from the coming soon page I enhanced, so basically, backport those changes into the minisite

--- __website/views/Vi_Home.php
+++ __website/views/Vi_Home.php
@@ -1,11 +1,18 @@
 <section>
 	<div class="img-area">
-		<img class="img-responsive" src="<?=$api->getImage("/resources/logo.png")?>" alt="Antimatter Studios" />
+		<img class="img-responsive" src="<?=$api->getFile("/resources/logo.png")?>" alt="Antimatter Studios" />
 	</div>
 
 	<div class="txt-area">
-		<h1>We are running the amslib framework website</h1>
+		<h1>Welcome to the Amslib Mini-Site</h1>
 
-		<h2>Coming soon, in time we'll fill this with content</h2>
+		<ul>
+			<li><a href="/about/">About Framework</a></li>
+			<li><a href="/getting-started/">Getting Started</a></li>
+			<li><a href="/plugins/">What are plugins?</a></li>
+			<li><a href="/webservices-api/">Explanation of Webservices and API's</a></li>
+			<li><a href="/documentation/">Amslib Documentation</a></li>
+			<li><a href="/test-framework/">Testing Framework</a></li>
+		</ul>
 	</div>
 </section>
--- __website/views/Vi_Skeleton.php
+++ __website/views/Vi_Skeleton.php
@@ -2,21 +2,21 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <meta name="description" content="<?=$meta_description?>">
     <meta name="author" content="<?=$meta_author?>">
 
 	<title><?=$site_title?></title>
 
-	<link rel="shortcut icon" href="<?=$api->getImage("/favicon/favicon.ico")?>" type="image/x-icon" />
-	<link rel="apple-touch-icon" href="<?=$api->getImage("/favicon/apple-touch-icon.png")?>" />
-	<link rel="apple-touch-icon" sizes="57x57" href="<?=$api->getImage("/favicon/apple-touch-icon-57x57.png")?>" />
-	<link rel="apple-touch-icon" sizes="72x72" href="<?=$api->getImage("/favicon/apple-touch-icon-72x72.png")?>" />
-	<link rel="apple-touch-icon" sizes="76x76" href="<?=$api->getImage("/favicon/apple-touch-icon-76x76.png")?>" />
-	<link rel="apple-touch-icon" sizes="114x114" href="<?=$api->getImage("/favicon/apple-touch-icon-114x114.png")?>" />
-	<link rel="apple-touch-icon" sizes="120x120" href="<?=$api->getImage("/favicon/apple-touch-icon-120x120.png")?>" />
-	<link rel="apple-touch-icon" sizes="144x144" href="<?=$api->getImage("/favicon/apple-touch-icon-144x144.png")?>" />
-	<link rel="apple-touch-icon" sizes="152x152" href="<?=$api->getImage("/favicon/apple-touch-icon-152x152.png")?>" />
+	<link rel="shortcut icon" href="<?=$api->getFile("/favicon/favicon.ico")?>" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="<?=$api->getFile("/favicon/apple-touch-icon.png")?>" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?=$api->getFile("/favicon/apple-touch-icon-57x57.png")?>" />
+	<link rel="apple-touch-icon" sizes="72x72" href="<?=$api->getFile("/favicon/apple-touch-icon-72x72.png")?>" />
+	<link rel="apple-touch-icon" sizes="76x76" href="<?=$api->getFile("/favicon/apple-touch-icon-76x76.png")?>" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?=$api->getFile("/favicon/apple-touch-icon-114x114.png")?>" />
+	<link rel="apple-touch-icon" sizes="120x120" href="<?=$api->getFile("/favicon/apple-touch-icon-120x120.png")?>" />
+	<link rel="apple-touch-icon" sizes="144x144" href="<?=$api->getFile("/favicon/apple-touch-icon-144x144.png")?>" />
+	<link rel="apple-touch-icon" sizes="152x152" href="<?=$api->getFile("/favicon/apple-touch-icon-152x152.png")?>" />
 
 	<?=Amslib_Resource::getStylesheet()?>
 	<?=Amslib_Resource::getJavascript()?>
@@ -26,22 +26,12 @@
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="glyphicon glyphicon-align-justify"></span>
-				</button>
-
-                <a class="navbar-brand" href="<?=$url_home?>"><?=$site_title?></a>
+                <a class="navbar-brand" href="<?=$url_home?>">Antimatter Studios</a>
             </div>
         </div>
-        <!-- /.container -->
     </nav>
 
-    <main>
-		<!--[if lt IE 7]>
-		<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-		<![endif]-->
-
+    <main class="vcenter">
 		<?=$content?>
     </main>
 </body>
