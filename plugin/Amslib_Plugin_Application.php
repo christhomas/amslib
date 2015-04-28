eb49b5e698572f7cc300b69608a02d3e5d43ac0e
Chris Thomas <chris.thomas@antimatter-studios.com>
2015-04-28 21:43:10 +0200

    delete a \ at the beginning of the Amslib_Plugin_Application.php file

--- plugin/Amslib_Plugin_Application.php
+++ plugin/Amslib_Plugin_Application.php
@@ -1,4 +1,4 @@
-\<?php
+<?php
 /*******************************************************************************
  * Copyright (c) {15/03/2008} {Christopher Thomas}
  * This library is free software; you can redistribute it and/or
