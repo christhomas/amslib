5089979c75ade83b5c7cd38e8d97b22f3762af5a
Chris Thomas <chris.thomas@antimatter-studios.com>
2015-07-27 16:37:54 +0200

    add a callback functionality to the Amslib_Exception class so you can customise what it does with every error and exception after the default steps are done
    Signed-off-by: Chris Thomas <chris.thomas@antimatter-studios.com>

--- exception/Amslib_Exception.php
+++ exception/Amslib_Exception.php
@@ -2,6 +2,8 @@
 class Amslib_Exception extends Exception
 {
 	protected $data;
+	
+	static protected $callback = false;
 
 	public function __construct($message,$data=array())
 	{
@@ -13,6 +15,18 @@ class Amslib_Exception extends Exception
 
 		//	NOTE: I have to test whether this records the correct location in all circumstances
 		$this->setData("location",Amslib_Debug::getCodeLocation(3));
+		
+		//	if this callback is usable, call it for the custom functionality
+		if(is_callable(self::$callback)){
+			call_user_func(self::$callback,$message,$data);
+		}
+	}
+	
+	static public function setCallback($callback)
+	{
+		if(is_callable($callback)){
+			self::$callback = $callback;
+		}
 	}
 
 	public function setData($key,$value)
