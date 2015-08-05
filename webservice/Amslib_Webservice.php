9b89655988b247bb5b2c32c1923346a7ee26aa62
Chris Thomas <chris.thomas@antimatter-studios.com>
2015-08-05 23:24:59 +0200

    I'll add these rules to test whether it works, then perhaps I'll make it an option to enable depending on the scenario

--- webservice/Amslib_Webservice.php
+++ webservice/Amslib_Webservice.php
@@ -11,7 +11,7 @@ class Amslib_Webservice
 	public function __construct($location,$params=array())
 	{
 		$this->setRouterURLSource("Amslib_Router_URL");
-		
+
 		$this->setLocation($location);
 		$this->setParams($params);
 	}
@@ -28,46 +28,46 @@ class Amslib_Webservice
 	public function setRouterURLSource($source=NULL)
 	{
 		//	There might be other conditions here which determine whether this can run or not
-		
+
 		if($source && is_callable($source,"getServiceURL")){
 			$this->router_url_source = $source;
 		}
 	}
-	
+
 	public function setLocation($location)
 	{
 		$this->url = false;
-		
+
 		if(!$this->url) $this->setRoute($location);
 		if(!$this->url) $this->setURL($location);
 	}
-	
+
 	public function setURL($url)
 	{
 		$this->url = $url;
 	}
-	
+
 	public function setRoute($route)
 	{
 		$callback = array($this->router_url_source,"getServiceName");
-		
+
 		if(is_callable($callback)){
 			$this->url = call_user_func($callback,$route);
 		}
 	}
-	
+
 	public function setParams($params)
 	{
 		if(is_array($params) && !empty($params)){
 			$this->params = $params;
 		}
 	}
-	
+
 	public function setParam($key,$value)
 	{
 		$this->params[$key] = $value;
 	}
-	
+
 	public function setAuth($username=NULL,$password=NULL)
 	{
 		if(is_string($username) && strlen($username) && is_string($password) && strlen($password)){
@@ -83,33 +83,39 @@ class Amslib_Webservice
 	{
 		try{
 			if(strlen($this->url) == 0) throw new Exception("webservice url was invalid");
-		
+
 			$curl = curl_init();
-			
+
 			$params = http_build_query(Amslib_Array::valid($this->params));
-			
+
 			if($this->username && $this->password){
 				curl_setopt($curl,CURLOPT_USERPWD,"$this->username:$this->password");
 			}
-			
+
 			curl_setopt($curl,CURLOPT_URL,				$this->url);
 			curl_setopt($curl,CURLOPT_POST,				true);
 			curl_setopt($curl,CURLOPT_HTTP_VERSION,		1.0);
 			curl_setopt($curl,CURLOPT_RETURNTRANSFER,	true);
 			curl_setopt($curl,CURLOPT_HEADER,			false);
 			curl_setopt($curl,CURLOPT_POSTFIELDS,		$params);
-			
+
+			//	FIXME: perhaps we shouldn't disable these? it's maybe a security problem
+			if (strpos ($this->url, 'https://') !== false){
+				curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, 0);
+				curl_setopt ($curl, CURLOPT_SSL_VERIFYHOST, 0);
+			}
+
 			$this->reply = curl_exec($curl);
-			
+
 			if(!$this->reply || !strlen($this->reply)){
 				Amslib_Debug::log("CURL ERROR",curl_error($curl),Amslib_Debug::dump($this->reply));
 				curl_close($curl);
-			
+
 				return false;
 			}
-			
+
 			curl_close($curl);
-			
+
 			return true;
 		}catch(Exception $e){
 			$exception = $e->getMessage();
@@ -124,19 +130,19 @@ class Amslib_Webservice
 
 		return false;
 	}
-	
+
 	public function getResponse($type)
 	{
 		$type = strtolower($type);
-		
+
 		$map = array(
 			"amslib"	=>	"Amslib_Webservice_Response_Amslib",
 			"json"		=>	"Amslib_Webservice_Response_JSON",
 			"raw"		=>	"Amslib_Webservice_Response_Raw"
 		);
-		
+
 		$type = in_array($type,array("amslib","json","raw")) ? $map[$type] : $map["raw"];
-		
+
 		return new $type($this->reply);
 	}
 }
