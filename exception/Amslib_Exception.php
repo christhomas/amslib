c03ace31a71027de671ed11afb3b2e993be8ad4c
Chris Thomas <chris.thomas@antimatter-studios.com>
2015-08-15 01:52:40 +0200

    allow any scalar non-zero-length value to be a key, I think this will work better

--- exception/Amslib_Exception.php
+++ exception/Amslib_Exception.php
@@ -2,7 +2,7 @@
 class Amslib_Exception extends Exception
 {
 	protected $data;
-	
+
 	static protected $callback = false;
 
 	public function __construct($message,$data=array(),$log_entry=true)
@@ -15,13 +15,13 @@ class Amslib_Exception extends Exception
 
 		//	NOTE: I have to test whether this records the correct location in all circumstances
 		$this->setData("location",basename($this->getFile())."@".$this->getLine());
-		
+
 		//	if this callback is usable, call it for the custom functionality
 		if(is_callable(self::$callback) && $log_entry){
 			call_user_func(self::$callback,$this,Amslib_Debug::getStackTrace("type","text"));
 		}
 	}
-	
+
 	static public function setCallback($callback)
 	{
 		self::$callback = $callback;
@@ -29,14 +29,14 @@ class Amslib_Exception extends Exception
 
 	public function setData($key,$value)
 	{
-		return is_string($key) && strlen($key)
+		return is_scalar($key) && strlen($key)
 			? $this->data[$key] = $value
 			: NULL;
 	}
 
 	public function getData($key=NULL)
 	{
-		if(!is_string($key) || !strlen($key)){
+		if(!is_scalar($key) || !strlen($key)){
 			return $this->data;
 		}else if(!isset($this->data[$key])){
 			return NULL;
