587296f33ab91784730aaeb8b95eef47b2993799
Chris Thomas <chris.thomas@antimatter-studios.com>
2015-09-05 21:46:24 +0200

    update the website bootstrap script so it searches for matching paths backwards until it runs out of places to search, but should match something

--- __website/amslib_router.php
+++ __website/amslib_router.php
@@ -1,7 +1,16 @@
 <?php
-$path = dirname(__DIR__);
+$path = $search = dirname(__DIR__);
 
-require_once("$path/vendor/autoload.php");
+do{
+	$file = "$search/vendor/autoload.php";
+
+	if(file_exists($file)){
+		require_once($file);
+		break;
+	}else if(!strlen($search)){
+		break;
+	}
+}while($search = rtrim(dirname($search),"/"));
 
 $application = new Amslib_Plugin_Application("application",$path);
 $application->setDebug(true);
